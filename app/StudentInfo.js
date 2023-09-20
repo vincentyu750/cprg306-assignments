ddfb624b95ccdbfee456a1ab163ae516569e41c5
Vincent Yu <70932487+vincentyu750@users.noreply.github.com>
2023-09-20 12:12:53 -0600

    Creating Week 2 Assignment
    root page and week 2 page

---
+++ app/StudentInfo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h2>Vincent Yu</h2>
+            <h2>CPRG 306 C</h2>
+            <Link href={"https://github.com/vincentyu750/cprg306-assignments"}>Github Link</Link>
+        </div>
+    )
+       
+}
+
---
+++ app/week2/page.js
@@ -0,0 +1,11 @@
+import StudentInfo from "../StudentInfo"
+import Link from "next/link"
+
+export default function Home() {
+  return (
+    <main>
+      <h1>My Shopping List</h1>
+      <StudentInfo/>
+    </main>
+  )
+}
